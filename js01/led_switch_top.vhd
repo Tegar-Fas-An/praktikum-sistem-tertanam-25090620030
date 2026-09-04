09a44d3a1d84f8a17287f842f15433b789f2ba61
Tegar-Fas-An <orotna123@gmail.com>
2026-09-04 21:53:41 +0700

    js01: praktikum

---
+++ js01/led_switch_top.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/03/2026 08:38:43 PM
+-- Design Name: 
+-- Module Name: led_switch_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity led_switch_top is
+    Port ( sw : in std_logic_vector (15 downto 0);
+           led : out std_logic_vector (15 downto 0));
+end led_switch_top;
+
+architecture Behavioral of led_switch_top is
+
+begin
+    --rangkaian kombinasional: setiap led mengikuti kondisi switch yang bersesuaian
+    led <= sw;
+
+end Behavioral;
